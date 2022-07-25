393ca76819875421470d1608769468f7ce8d0552
Akmaljon Gafforov <16236077+agafforov@users.noreply.github.com>
2022-07-25 18:30:59 +0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Rules;
+
+interface CostCalculatorInterface
+{
+    public function getCost(): float;
+}
+
+trait CostCalculatorTrait
+{
+    private CostCalculatorInterface $previousCostCalculator;
+
+    public function __construct(CostCalculatorInterface $previousCostCalculator)
+    {
+        $this->previousCostCalculator = $previousCostCalculator;
+    }
+}
+
+class BasicInspectionCostCalculator implements CostCalculatorInterface
+{
+    public function getCost(): float
+    {
+        return 25;
+    }
+}
+
+class TireReplacementCostCalculator implements CostCalculatorInterface
+{
+    use CostCalculatorTrait;
+
+    public function getCost(): float
+    {
+        return 25 + $this->getCost();
+    }
+}
+
+class PaintingCarCostCalculator implements CostCalculatorInterface
+{
+    use CostCalculatorTrait;
+
+    public function getCost(): float
+    {
+        return 150 + $this->getCost();
+    }
+}
+
+$totalCost = new PaintingCarCostCalculator(new TireReplacementCostCalculator(new BasicInspectionCostCalculator()));
+
